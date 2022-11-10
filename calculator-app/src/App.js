4edc1d103d0398c6fa66c09f6518fa53f0364238
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-11-11 02:17:42 +0530

    done with the calculator app

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -6,8 +6,27 @@ function App() {
   const[result,setResult] = useState("")
 
   const handleClick = (e) => {
-   setResult()a
+    setResult(result.concat(e.target.value))
+    console.log(result.concat(e.target.value))
   }
+
+  const clear = () => {
+    setResult("")
+  }
+
+  const backsapce = () => {
+    setResult(result.slice(0, -1));
+  }
+
+  const calculate = () => {
+
+    let value = eval(result)
+   setResult(value.toString())
+  console.log(eval(result))
+    
+  }
+
+
   return (
     <>
     <div className="container">
@@ -16,24 +35,24 @@ function App() {
          </form>
 
        <div className="keypad">
-         <button id="clear"  >AC</button>
-         <button id="backspace">C</button>
-         <button className="symbols" value="/" >/</button>
-         <button className='digits' value="7" >7</button>
-         <button className='digits' value="8" >8</button>
-         <button className='digits' value="9" >9</button>
-         <button className="symbols" value="*" >x</button>
-         <button className='digits' value="4" >4</button>
-         <button className='digits' value="5" >5</button>
-         <button className='digits' value="6" >6</button>
-         <button className="symbols" value="-" >-</button>
-         <button className='digits' value="1" >1</button>
-         <button className='digits' value="2" >2</button>
-         <button className='digits' value="3" >3</button>
-         <button className="symbols" value="+" >+</button>
-         <button className='digits' value="0" >0</button>
-         <button className='digits' value="." >.</button>
-         <button id='submit' >=</button>
+         <button id="clear" onClick={clear} >AC</button>
+         <button onClick={backsapce} id="backspace">C</button>
+         <button className="symbols" value="/" onClick={handleClick}>/</button>
+         <button className='digits' value="7" onClick={handleClick}>7</button>
+         <button className='digits' value="8" onClick={handleClick}>8</button>
+         <button className='digits' value="9" onClick={handleClick}>9</button>
+         <button className="symbols" value="*" onClick={handleClick}>x</button>
+         <button className='digits' value="4" onClick={handleClick}>4</button>
+         <button className='digits' value="5" onClick={handleClick}>5</button>
+         <button className='digits' value="6" onClick={handleClick}>6</button>
+         <button className="symbols" value="-" onClick={handleClick}>-</button>
+         <button className='digits' value="1" onClick={handleClick}>1</button>
+         <button className='digits' value="2" onClick={handleClick}>2</button>
+         <button className='digits' value="3" onClick={handleClick}>3</button>
+         <button className="symbols" value="+" onClick={handleClick}>+</button>
+         <button className='digits' value="0" onClick={handleClick}>0</button>
+         <button className='digits' value="." onClick={handleClick}>.</button>
+         <button id='submit' onClick={calculate}>=</button>
 
        </div>
     </div>
