454314b19a6bcecaf5baf2b3114c2df4b54db47b
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-11-11 02:02:00 +0530

    done with the UI part

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,14 +1,43 @@
-
 import './App.css';
+import React , { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       hello world
 
-      </header>
+  const[result,setResult] = useState("")
+
+  const handleClick = (e) => {
+   setResult()
+  }
+  return (
+    <>
+    <div className="container">
+         <form>
+           <input type="text" value={result}/>
+         </form>
+
+       <div className="keypad">
+         <button id="clear"  >AC</button>
+         <button id="backspace">C</button>
+         <button className="symbols" value="/" >/</button>
+         <button className='digits' value="7" >7</button>
+         <button className='digits' value="8" >8</button>
+         <button className='digits' value="9" >9</button>
+         <button className="symbols" value="*" >x</button>
+         <button className='digits' value="4" >4</button>
+         <button className='digits' value="5" >5</button>
+         <button className='digits' value="6" >6</button>
+         <button className="symbols" value="-" >-</button>
+         <button className='digits' value="1" >1</button>
+         <button className='digits' value="2" >2</button>
+         <button className='digits' value="3" >3</button>
+         <button className="symbols" value="+" >+</button>
+         <button className='digits' value="0" >0</button>
+         <button className='digits' value="." >.</button>
+         <button id='submit' >=</button>
+
+       </div>
     </div>
+ </>
   );
 }
 
